fb3d214dd956a4c6da53b2fd2494042507463f63
Keon <55180628+keon981@users.noreply.github.com>
2021-01-19 12:59:44 +0800

    程式碼

---
+++ 3D_End_106051981/Assets/scripts/NPCData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC 資料", menuName = "keon/NPC 資料")]
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialougA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+    [Header("任務需求數量")]
+    public int count;
+    [Header("已完成數量")]
+    public int countCurrent;
+}
