35cc79368a0daebbee30a00c4133558bf1e29de6
Keon <55180628+keon981@users.noreply.github.com>
2021-01-19 20:30:38 +0800

    bug一大堆

--- 3D_End_106051981/Assets/scripts/enemy.cs
+++ 3D_End_106051981/Assets/scripts/enemy.cs
@@ -7,11 +7,13 @@ public class enemy : MonoBehaviour
     [Header("移動速度"), Range(0, 50)]
     public float speed = 3;
     [Header("停止距離"), Range(0, 50)]
-    public float stopDistance = 2.5f;
+    public float stopDistance = 2f;
 
     private Transform player;
     private NavMeshAgent nav;
     private Animator ani;
+
+    private float timer;
 
     private void Awake()
     {
@@ -27,13 +29,26 @@ public class enemy : MonoBehaviour
     private void Update()
     {
         Track();
+        Skill();
     }
 
+
+    private void Skill()
+    {
+        if(nav.remainingDistance < stopDistance)
+        {
+
+            ani.SetTrigger("跳舞觸發");
+
+        }
+    }
+
+    //追蹤
     private void Track()
     {
         nav.SetDestination(player.position);
 
         // print("剩餘的距離:" + nav.remainingDistance);
-        ani.SetBool("跑步開關:", + nav.remainingDistance > stopDistance);
+        ani.SetBool("跑步開關", + nav.remainingDistance > stopDistance);
     }
 }
---
+++ 3D_End_106051981/Assets/scripts/player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;
+
+public class player : MonoBehaviour
+{
+    private float hp = 100;
+    private Animator ani;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public void Damage()
+    {
+        hp -= 35;
+        ani.SetTrigger("受傷觸發");
+
+
+
+    }
+
+}
